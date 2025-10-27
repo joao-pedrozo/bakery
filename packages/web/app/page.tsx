6ac9df96cdf1c4a6ea6aefbd1baeebfea49311b6
João Pedro <jp.pedrozo@hotmail.com>
2025-10-26 22:06:43 -0300

    feat: add conditional rendering

--- packages/web/app/page.tsx
+++ packages/web/app/page.tsx
@@ -1,25 +1,20 @@
 "use client";
 
-import Image from "next/image";
 import { useCakes } from "@/src/hooks/useCakes";
 
 export default function Home() {
-  const { data, error, isLoading } = useCakes();
+  const { data, status, error } = useCakes();
 
-  console.log(123, data, error, isLoading);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error</div>;
-  }
+  const content = {
+    pending: <div>Loading...</div>,
+    error: <div>Error: {error?.message}</div>,
+    success: <pre>{JSON.stringify(data, null, 2)}</pre>,
+  };
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <pre>{JSON.stringify(data, null, 2)}</pre>
+        {content[status]}
       </main>
     </div>
   );
